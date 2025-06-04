5ac0add6335820a517b988b5a6055d6659c25d9d
Ayida-Aman <amanayida1@gmail.com>
2025-06-04 05:58:30 +0300

    Fix server binding for Render deployment

--- index.js
+++ index.js
@@ -31,7 +31,9 @@ app.post("/payments/create", async (req, res) => {
   }
 });
 
-app.listen(5000, (err) => {
-  if (err) throw err;
-  console.log("Amazon running on PORT:5000, http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(
+    `Amazon running on PORT:${PORT}, accessible at http://localhost:${PORT}`
+  );
 });
